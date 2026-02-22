5033c9b7b54c5f504f1c68e76dceec170614ffae
BishalChattoraj <bishalchattoraj@gmail.com>
2026-02-22 12:49:01 +0530

    First message

---
+++ index.mjs
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "🚀 Jenkins + Docker Automation Successful!",
+        timestamp: new Date()
+    });
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).send("OK");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
