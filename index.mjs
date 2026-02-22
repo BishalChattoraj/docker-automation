80a8b7ab0097aaf1e232c631bdba68d579dabf5f
BishalChattoraj <bishalchattoraj@gmail.com>
2026-02-22 15:27:39 +0530

    index file changed

--- index.mjs
+++ index.mjs
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.json({
-        message: "🚀 Jenkins + Docker Automation Successful,yeah!",
+        message: "🚀 Jenkins + Docker Automation Successful!",
         timestamp: new Date()
     });
 });
